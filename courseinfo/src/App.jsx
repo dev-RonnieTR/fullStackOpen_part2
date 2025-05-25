316114eb14e60ee21a6a801a8a7651882de9c765
Ronaldo <ronaldoteran2407@gmail.com>
2025-05-25 14:47:57 -0500

    courseinfo: create <Course/> component and have <App/> render <Course/>. Make appropriate changes in the rest of the code to ensure functionality

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+const Course = ({ course }) => {
+	return (
+		<>
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+		</>
+	);
+};
+
+const Header = ({ course }) => {
+	return <h1>{course}</h1>;
+};
+
+const Content = ({ parts }) => {
+	return (
+		<>
+			{parts.map((part) => (
+				<Part key={part.id} name={part.name} exercises={part.exercises} />
+			))}
+		</>
+	);
+};
+
+const Part = ({ name, exercises }) => {
+	return (
+		<p>
+			{name}: {exercises}
+		</p>
+	);
+};
+
+const Total = ({ parts }) => {
+	const totalExcercises = parts.reduce((acc, part) => acc + part.exercises, 0);
+
+	return <p>Number of exercises: {totalExcercises}</p>;
+};
+
+const App = () => {
+	const course = {
+		id: 1,
+		name: "Half Stack application development",
+		parts: [
+			{ id: 1, name: "Fundamentals of React", exercises: 10 },
+			{ id: 2, name: "Using props to pass data", exercises: 7 },
+			{ id: 3, name: "State of a component", exercises: 14 },
+		],
+	};
+
+	return (
+		<>
+			<Course course={course} />
+		</>
+	);
+};
+
+export default App;
