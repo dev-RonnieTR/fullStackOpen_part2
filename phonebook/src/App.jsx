0b9700cd3eff4824f5d77ebe14cce693553b112c
Ronaldo <ronaldoteran2407@gmail.com>
2025-05-26 10:48:26 -0500

    phonebook: add meaningful console logs to the code

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -13,19 +13,31 @@ const Form = ({ persons, setPersons }) => {
 	const [values, setValues] = useState(newData); //State that will work as the onChange handler for every text input
 
 	const handleChange = (event) => {
-		setValues((prevState) => ({
-			...prevState,
-			[event.target.id]: event.target.value,
-		}));
+		console.log(`Input changed: ${event.target.id} = ${event.target.value}`);
+		setValues((prevState) => {
+			const newState = {
+				...prevState,
+				[event.target.id]: event.target.value,
+			};
+			console.log("Updated form state:", newState);
+			return newState;
+		});
 	};
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		if (persons.some((person) => person.name === values.name)) {
+			console.warn(`${values.name} is already in the phonebook`);
 			alert(`${values.name} is already added to phonebook`);
 			return;
 		}
-		setPersons((prevState) => [...prevState, values]);
+		console.log("Submitting form with values:", values);
+
+		setPersons((prevState) => {
+			const updatedPersons = [...prevState, values];
+			console.log("Updated persons list:", updatedPersons);
+			return updatedPersons;
+		});
 	};
 
 	return (
