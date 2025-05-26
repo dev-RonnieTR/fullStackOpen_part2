6119975298d0c3d4242a5e8f16393e996ffff978
Ronaldo <ronaldoteran2407@gmail.com>
2025-05-26 10:39:15 -0500

    phonebook: add a number input. Also refactor the code to avoid hard-coding in a way so the developer can add as many inputs as they want easily just by changing the "data" array

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,37 +1,53 @@
 import { useState } from "react";
 
+const data = ["name", "number"]; //Array that contains the pieces of data to be collected from the person. They have to be used as the id for the inputs
+const newData = Object.fromEntries(data.map((data) => [data, ""]));
+//Returns an object where each key is an element from the data array, and each value is initialized to an empty string.
+// Example: newData = { name: "", number: "", ... }
+
+const capitalize = (word) => {
+	return word[0].toUpperCase() + word.slice(1);
+};
+
 const Form = ({ persons, setPersons }) => {
-	const [newName, setNewName] = useState("");
+	const [values, setValues] = useState(newData); //State that will work as the onChange handler for every text input
+
 	const handleChange = (event) => {
-		setNewName(event.target.value);
+		setValues((prevState) => ({
+			...prevState,
+			[event.target.id]: event.target.value,
+		}));
 	};
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		if (persons.some((person) => person.name === newName)) {
-			alert(`${newName} is already added to phonebook`);
+		if (persons.some((person) => person.name === values.name)) {
+			alert(`${values.name} is already added to phonebook`);
 			return;
 		}
-		setPersons((prevState) => [...prevState, { name: newName }]);
-		setNewName("");
+		setPersons((prevState) => [...prevState, values]);
 	};
+
 	return (
 		<form onSubmit={handleSubmit}>
-			<FormTextInput label="Name" onChange={handleChange} newName={newName} />
-			<SubmitButton text="Add" newName={newName} />
+			{data.map((data) => (
+				<FormTextInput
+					key={data}
+					id={data}
+					onChange={handleChange}
+					values={values}
+				/>
+			))}
+			<SubmitButton text="Add" values={values} />
 		</form>
 	);
 };
 
-const FormTextInput = ({ label, onChange, newName }) => {
+const FormTextInput = ({ id, onChange, values }) => {
 	return (
 		<div>
-			<label htmlFor={`${label}-input`}>{label}: </label>
-			<input
-				id={`${label}-input`}
-				type="text"
-				onChange={onChange}
-				value={newName}
-			/>
+			<label htmlFor={id}>{capitalize(id)}: </label>
+			<input id={id} type="text" onChange={onChange} value={values[id]} />
 		</div>
 	);
 };
@@ -45,16 +61,20 @@ const Numbers = ({ persons }) => {
 		<section>
 			<h2>Numbers</h2>
 			{persons.map((person) => (
-				<p key={person.name}>{person.name}</p>
+				<div key={person.name}>
+					{data.map((data) => (
+						<span key={`${person.name}-${data}`}>{person[data]} </span>
+					))}
+				</div>
 			))}
 		</section>
 	);
 };
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-	console.log(persons);
-
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", number: "040-1234567" },
+	]);
 	return (
 		<>
 			<h1>Phonebook</h1>
