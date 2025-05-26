4890a6b646fb1171be9e81af0f62ca71d8880eb6
Ronaldo <ronaldoteran2407@gmail.com>
2025-05-25 23:25:50 -0500

    phonebook: create the functionality to enter a name and display it in the numbers section

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const Form = ({ setPersons }) => {
+	const [newName, setNewName] = useState("");
+	const handleChange = (event) => {
+		setNewName(event.target.value);
+	};
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setPersons((prevState) => [...prevState, { name: newName }]);
+		setNewName("");
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<FormTextInput label="Name" onChange={handleChange} newName={newName} />
+			<SubmitButton text="Add" newName={newName} />
+		</form>
+	);
+};
+
+const FormTextInput = ({ label, onChange, newName }) => {
+	return (
+		<div>
+			<label htmlFor={`${label}-input`}>{label}: </label>
+			<input
+				id={`${label}-input`}
+				type="text"
+				onChange={onChange}
+				value={newName}
+			/>
+		</div>
+	);
+};
+
+const SubmitButton = ({ text }) => {
+	return <button type="submit">{text}</button>;
+};
+
+const Numbers = ({ persons }) => {
+	return (
+		<section>
+			<h2>Numbers</h2>
+			{persons.map((person) => (
+				<p key={person.name}>{person.name}</p>
+			))}
+		</section>
+	);
+};
+
+const App = () => {
+	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	console.log(persons);
+
+	return (
+		<>
+			<h1>Phonebook</h1>
+			<Form setPersons={setPersons} />
+			<Numbers persons={persons} />
+		</>
+	);
+};
+
+export default App;
