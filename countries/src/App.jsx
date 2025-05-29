15175ae96f2c9e7c2bee1eec05beda03c78a85ae
Ronaldo <ronaldoteran2407@gmail.com>
2025-05-29 16:05:12 -0500

    countries: create a countries state initialized to an empty array. Fetch all the countries, get their names into an array and set that array as the new state for countries after the App has rendered using useEffect

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,8 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { UserInput } from "./components";
+import countriesService from "./services/countries";
 
 const App = () => {
 	const [value, setValue] = useState("");
+	const [countries, setCountries] = useState([]);
+
+	const fetchCountries = async () => {
+		try {
+			console.log("Fetching countries...");
+			const response = await countriesService.getAll();
+			const countryNames = response.map((country) => country.name.common);
+			setCountries(countryNames);
+			console.log("Fetch successful");
+		} catch {
+			console.log("Fetch failed");
+		}
+	};
+
+	useEffect(() => {
+		fetchCountries();
+	}, []);
+
+	if (!countries.length) return; //Nothing renders if countries is empty.
 
 	return (
 		<>
